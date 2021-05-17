264036bb6c419fe42e108cedd19dc588a39cbcc2
Jacek <jacek@g.pl>
2021-05-17 11:22:38 +0200

    Problem no 09

--- src/projectEuler/Problem009.java
+++ src/projectEuler/Problem009.java
@@ -12,6 +12,20 @@ package projectEuler;
 public class Problem009 {
 
     public static void main(String[] args) {
+        for (int a = 1; a < 1000; a++) {
+            for (int b = 1; b < 1000; b++) {
+                int c = 1000 - a - b;
 
+                if (a * a + b * b == c * c) {
+                    System.out.println("Rozwiązanie: ");
+                    System.out.println("a = " + a);
+                    System.out.println("b = " + b);
+                    System.out.println("c = " + c);
+
+                    System.out.println("Rezultat: " + a * b * c);
+                }
+
+            }
+        }
     }
 }
